bb01b83a0baad10d9ce1ff358b6e8f9ab82c2359
bestups <49695252+bestups@users.noreply.github.com>
2025-03-24 20:28:30 +0100

    Delete src directory

--- src/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-// frontend-app/src/environments/environment.prod.ts
-export const environment = {
-    production: true,
-    apiUrl: 'https://twoja-domena.com'
-  };
-  
--- src/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-// frontend-app/src/environments/environment.ts
-
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080'
-  };
-  
